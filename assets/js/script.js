c2d7b9abe81c362bd590baf4c33f09efcec1375e
Aar Kaiser <studies@mohussein.com>
2021-09-30 09:42:39 -0400

    updated template

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// Sidebar Element
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     let elems = document.querySelectorAll('.sidenav');
+//     let instances = M.Sidenav.init(elems, options);
+//   });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown letiation)
+  // let collapsibleElem = document.querySelector('.collapsible');
+  // let collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  // End of sidebar.
