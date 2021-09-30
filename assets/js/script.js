730fbb9e8e3af74a25048281712eb8aef12728cb
Aar Kaiser <studies@mohussein.com>
2021-09-30 14:41:07 -0400

    updates to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,5 @@
+// Aar //
 
-// Sidebar Element
+baseURL = "https://apewisdom.io/api/v1.0/filter/{filter}";
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     let elems = document.querySelectorAll('.sidenav');
-//     let instances = M.Sidenav.init(elems, options);
-//   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown letiation)
-  // let collapsibleElem = document.querySelector('.collapsible');
-  // let collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  // End of sidebar.
+document.baseURL;
